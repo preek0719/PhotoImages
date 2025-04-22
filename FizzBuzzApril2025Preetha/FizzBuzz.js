c7398591ca7ff33ea115db3e443bdec982922c10
Preetha Krishnan <preetha.krishnan@softwire.com>
2025-04-22 14:32:31 +0100

    2nd commit to print Fizz at multiples of 3

--- FizzBuzzApril2025Preetha/FizzBuzz.js
+++ FizzBuzzApril2025Preetha/FizzBuzz.js
@@ -1,5 +1,12 @@
 //Print numbers from 1 to 100
+//Print Fizz whenever number is multiple of 3
 
 for (let i=1; i<=100; i++) {
+   if (i%3 == 0) {
+    console.log('Fizz');
+   }
+   else {
     console.log(i);
+   }
 }
+
