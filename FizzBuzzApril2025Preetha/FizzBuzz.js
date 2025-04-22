5c21ca1ebb9834185bc55719e690074618bb46f8
Preetha Krishnan <preetha.krishnan@softwire.com>
2025-04-22 14:40:27 +0100

    3rd commit to print Buzz at multiples of 5 and FizzBuzz at multiples of 3 and 5

--- FizzBuzzApril2025Preetha/FizzBuzz.js
+++ FizzBuzzApril2025Preetha/FizzBuzz.js
@@ -1,9 +1,18 @@
-//Print numbers from 1 to 100
-//Print Fizz whenever number is multiple of 3
+/* From 1 to 100
+Print Fizz whenever number is multiple of 3
+Print Buzz whenever number is multiple of 5
+Print FizzBuzz whenever number is multiple of 3 and 5
+Print number when above conditions are not satisfied */
 
 for (let i=1; i<=100; i++) {
-   if (i%3 == 0) {
+   if (i%3 == 0 && i%5 == 0) {
     console.log('Fizz');
+   }
+   else if (i%3 == 0) {
+    console.log('Buzz');
+   }
+   else if (i%5 == 0) {
+    console.log('FizzBuzz');
    }
    else {
     console.log(i);
