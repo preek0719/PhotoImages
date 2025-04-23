7121a31e71a4d7333c6c1ed1604e2d688a6aa0ca
Preetha Krishnan <preetha.krishnan@softwire.com>
2025-04-23 10:51:25 +0100

    first commit

---
+++ EmailExtraction/README.md
@@ -0,0 +1,2 @@
+//
+1) Download file in current working directory
--- FizzBuzzApril2025Preetha/FizzBuzz.js
+++ FizzBuzzApril2025Preetha/FizzBuzz.js
@@ -4,22 +4,31 @@ Print Buzz whenever number is multiple of 5
 Print FizzBuzz whenever number is multiple of 3 and 5
 Print number when above conditions are not satisfied */
 
-for (let i=1; i<=200; i++) {
-   if (i%3 == 0 && i%5 == 0) {
-    console.log('FizzBuzz');
-   }
-   
-   
-   else if (i%3 == 0) {
-    console.log('Fizz');
-   }
-   else if (i%5 == 0) {
-    console.log('Buzz');
-   }
-   
-  
-   else {
-    console.log(i);
-   }
+
+
+function returnPhrase(number) {
+    let phrase = '';
+    for (let i=1; i<=number; i++) {
+        phrase = calculatePhrase(i);
+        console.log(phrase);
+    }
 }
+
+function calculatePhrase(number) {
+    if(number%3 == 0 && number%5 == 0) {
+        return 'FizzBuzz';
+    }
+    else if(number%3 == 0) {
+        return 'Fizz' ;
+    }
+    else if (number%5 == 0) {
+        return 'Buzz';
+       
+    }
+
+    return number;   
+}
+
+returnPhrase(15);
+
 
